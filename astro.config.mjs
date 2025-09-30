9733f11bcae53b9d7da9770069e86291c313e313
SibuNDZ <doc.ndz@gmail.com>
2025-09-30 23:32:28 +0200

    updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+});
